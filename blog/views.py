83a3c5cb2ed64c01583f2eec71d4d2a61e235317
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2018-09-29 06:54:57 +0600

    added about view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from blog.models import Post, Comment
+from django.views.generic import (TemplateView, ListView)
+from django.utils import timezone
 
 # Create your views here.
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
